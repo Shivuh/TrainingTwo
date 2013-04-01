e82e0b48cc5e1a4c2ce5916a6a6a78ffbe396015
shivaleela <shivaleela.hiremath@gmail.com>
2013-04-01 11:22:34 +0530

    training two

---
+++ src/main/java/com/wipro/atlassian/TrainingPartTwo.java
@@ -0,0 +1,9 @@
+package com.wipro.atlassian;
+
+public class TrainingPartTwo {
+public String display()
+{
+	//System.out.print("Hello");
+	return ("Hello");
+}
+}
---
+++ src/test/java/com/wipro/test/AppTest.java
@@ -0,0 +1,12 @@
+package com.wipro.test;
+
+import junit.framework.TestCase;
+
+
+public class AppTest extends TestCase{
+	public void test(){
+		
+	}
+
+	
+}
